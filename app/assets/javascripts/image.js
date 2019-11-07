ee3eee54ea4815b34dbf82ed8fda576a5f2835f4
lllverb <wktkm9orz2@gmail.com>
2019-11-07 12:10:31 +0900

    微調整

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -5,14 +5,14 @@ document.addEventListener("turbolinks:load", function() {
   const el = document.querySelector('img');
   const radius = 5;
 
-  setInterval(() => {
-    const t = new Date().getTime() / 1500;
+  // setInterval(() => {
+  //   const t = new Date().getTime() / 1500;
 
-    const x =(Math.cos(t) * radius);
-    const X =(Math.sin(t) * radius * 5);
-    const y =(Math.sin(t) * radius);
-    const Y =(Math.cos(t) * radius * 5);
+  //   const x =(Math.cos(t) * radius);
+  //   const X =(Math.sin(t) * radius * 5);
+  //   const y =(Math.sin(t) * radius);
+  //   const Y =(Math.cos(t) * radius * 5);
 
-    el.setAttribute('style', `transform: skewX(${x}deg) skewY(${-x}deg) rotateX(${X}deg) rotateY(${Y}deg);`)
-  }, 20);
+  //   el.setAttribute('style', `transform: skewX(${x}deg) skewY(${-x}deg) rotateX(${X}deg) rotateY(${Y}deg);`)
+  // }, 20);
 })
